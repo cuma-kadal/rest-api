e7a354b62904adb3473e04fb99e83f470408c218
chikachan <136344173+cuma-kadal@users.noreply.github.com>
2024-02-21 17:35:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require("express");
 
 const app = express();
 const PORT = 4000;
-
+const proses_message = {
+  creator: 'RynXD',
+  message: 'Request akan segera diexecute, pantau secara berkala.'
+};
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
 });
 
 app.get("/", (req, res) => {
-  res.send("Hey this is my API running ?");
+  res.setHeader('Content-Type', 'application/json');
+ res.status(400).send(JSON.stringify(proses_message, null, 2));
 });
 
 app.get("/about", (req, res) => {
